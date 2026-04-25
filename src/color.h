2428e1fff65ba8e76f85e2ea9817ee340f1eba10
qscheetz <124484154+Quentin2006@users.noreply.github.com>
2026-04-25 16:12:45 -0700

    added rays

--- src/color.h
+++ src/color.h
@@ -1,21 +1,10 @@
 #include <cstdint>
 #include <glm/glm.hpp>
 
-class color : public glm::vec3 {
+using color = glm::vec3;
 
-public:
-  // this will inherit vecs constructor
-  using glm::vec3::vec;
-
-  /**
-   * @brief will convert color to a hex value
-   *
-   * @param color float [0-1]
-   * @return hex value
-   */
-  uint32_t convert() const {
-    return (static_cast<uint32_t>(r * 255.0f) << 16) |
-           (static_cast<uint32_t>(g * 255.0f) << 8) |
-           static_cast<uint32_t>(b * 255.0f);
-  }
-};
+uint32_t convert(color &c) {
+  return (static_cast<uint32_t>(c.r * 255.0f) << 16) |
+         (static_cast<uint32_t>(c.g * 255.0f) << 8) |
+         static_cast<uint32_t>(c.b * 255.0f);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include "color.h"
 #include "consts.h"
+#include "ray.h"
 #include "window.h"
 
 #include <array>
+#include <glm/geometric.hpp>
 #include <glm/glm.hpp>
 #include <iostream>
+
+color ray_color(const ray &r) {
+  vec3 unit_direction = glm::normalize(r.direction());
+  float a = 0.5 * (unit_direction.y + 1.0);
+  return (1.0f - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
@@ -12,16 +20,37 @@ int main() {
 
   std::array<uint32_t, window::SIZE> pixels;
 
+  point3 camera_center(0, 0, 0);
+
+  // Calculate the vectors across the horizontal and down the vertical viewport
+  // edges.
+  auto viewport_u = glm::vec3(camera::VIEWPORT_WIDTH, 0, 0);
+  auto viewport_v = glm::vec3(0, -camera::VIEWPORT_HEIGHT, 0);
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  auto pixel_delta_u = viewport_u / static_cast<float>(window::WIDTH);
+  auto pixel_delta_v = viewport_v / static_cast<float>(window::HEIGHT);
+
+  // Calculate the location of the upper left pixel.
+  auto viewport_upper_left = camera_center - vec3(0, 0, camera::FOCAL_LENGTH) -
+                             viewport_u / 2.f - viewport_v / 2.f;
+  auto pixel00_loc =
+      viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
+
   while (!window.should_close()) {
 
     for (int j = 0; j < window::HEIGHT; j++) {
       std::clog << "\rScanlines remaining: " << (window::HEIGHT - j) << ' '
                 << std::flush;
       for (int i = 0; i < window::WIDTH; i++) {
-        color pixel_color = color(double(i) / (window::WIDTH - 1),
-                                  double(j) / (window::HEIGHT - 1), 0);
+        auto pixel_center = pixel00_loc + ((float)i * pixel_delta_u) +
+                            ((float)j * pixel_delta_v);
+        auto ray_direction = pixel_center - camera_center;
+        ray r(camera_center, ray_direction);
 
-        pixels[j * window::WIDTH + i] = pixel_color.convert();
+        color pixel_color = ray_color(r);
+
+        pixels[j * window::WIDTH + i] = convert(pixel_color);
       }
     }
 
